5ec63af842923a9db8a6911c98dcedbc763055f2
10000DOO <gunjoun99@gmail.com>
2023-08-24 11:49:23 +0900

    Initial Commit

---
+++ NiceFilmTicket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NiceFilmTicket
+//
+//  Created by 10000DOO on 2023/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
