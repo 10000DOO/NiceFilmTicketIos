6a7e7f0f50761b55499a8ec20f19375ca82590b3
10000DOO <gunjoun99@gmail.com>
2023-08-27 12:56:30 +0900

    fix : 유사UI추출(git 문제 해결)
    유사 UI class로 추출 & git 문제 해결

---
+++ NiceFilmTicket/Features/Authentication/UIComponent/AuthenticationUIButton.swift
@@ -0,0 +1,26 @@
+//
+//  AuthenticationUIButton.swift
+//  NiceFilmTicket
+//
+//  Created by 10000DOO on 2023/08/27.
+//
+
+import UIKit
+
+class AuthenticationUIButton: UIButton {
+
+    init(title : String){
+        super.init(frame: .zero)
+        self.backgroundColor = UIColor(red: 8/255, green: 30/255, blue: 92/255, alpha: 1)
+        self.setTitle(title, for: .normal)
+        self.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20) // 텍스트 크기 설정
+        self.layer.cornerRadius = 10 // 테두리 둥글기 설정
+        self.layer.borderWidth = 1 // 테두리 두께 설정
+        self.layer.borderColor = UIColor(red: 8/255, green: 30/255, blue: 92/255, alpha: 1).cgColor // 테두리 색상 설정
+        self.isHidden = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("AuthenticationUIButton : init(coder:) has not been implemented")
+    }
+}
